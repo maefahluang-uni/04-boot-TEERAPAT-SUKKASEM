4b7a484d669c9aea166a7e3fbaca1d7783da1045
TEERAPAT-SUKKASEM <6731503015@lamduan.mfu.ac.th>
2025-09-01 09:45:34 +0000

    LAB SPRING BOOT Committed

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -17,20 +17,27 @@ public class UserController {
 
     public static Map<String, User> users = new HashMap<String, User>();
 
-   
-    public ResponseEntity<String> registerUser( User user) {
-      //TODO
-      return null;
+    @PostMapping("/users")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        if (users.containsKey(user.getUsername())) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
+        }
+        users.put(user.getUsername(), user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
 
+    @GetMapping("/users")
     public ResponseEntity<Collection<User>> list() {
-        //TODO
-        return null;
+        return new ResponseEntity<>(users.values(), HttpStatus.OK);
     }
 
-    public ResponseEntity<User> getUser(String username) {
-        //TODO
-        return null;
+    @GetMapping("/users/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User user = users.get(username);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
     
 }
